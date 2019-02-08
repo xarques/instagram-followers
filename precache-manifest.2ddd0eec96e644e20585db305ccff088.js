9872ba6ae0d62e6a3a35ee035fd92ed8a2b24d57
Xavier Arques <xavarques@gmail.com>
2019-02-08 18:48:11 +0100

    Updates

---
+++ precache-manifest.2ddd0eec96e644e20585db305ccff088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cd7ac45926cf425c72c",
+    "url": "/instagram-followers/static/css/main.d544b1f6.chunk.css"
+  },
+  {
+    "revision": "6cd7ac45926cf425c72c",
+    "url": "/instagram-followers/static/js/main.6cd7ac45.chunk.js"
+  },
+  {
+    "revision": "b5468bf7daf57a698beb",
+    "url": "/instagram-followers/static/js/1.b5468bf7.chunk.js"
+  },
+  {
+    "revision": "f9e3b5bf53400fc36e85",
+    "url": "/instagram-followers/static/js/runtime~main.f9e3b5bf.js"
+  },
+  {
+    "revision": "202986d2fc45e1c67dea73877e415a9d",
+    "url": "/instagram-followers/index.html"
+  }
+];
